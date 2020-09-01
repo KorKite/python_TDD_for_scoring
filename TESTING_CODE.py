74bb7f60004c25506ef3d1e51c5db97ecb9db5de
Junseo Ko <50725139+KorKite@users.noreply.github.com>
2020-09-01 14:16:45 +0900

    여러 테스트 케이스 고려

--- TESTING_CODE.py
+++ TESTING_CODE.py
@@ -5,17 +5,36 @@ ps.add_argument("--data_path", type = str,
                 help = "write data path")
 
 args = ps.parse_args()
-DATA_PATH = args.data_path
 
+
+DATA_PATH = args.data_path
 FILE_LIST = os.listdir(DATA_PATH)
 
-for file in FILE_LIST:
-    file_path = os.path.join(DATA_PATH, file)
-    print("##########",file,"############")
-    os.system(f"python3 {file_path}")
+if not FILE_LIST[0].endswith(".py"):
+    FILE_LIST_COPY = FILE_LIST[:]
+    FILE_LIST = []
+    for temp in FILE_LIST_COPY:
+        inside = os.path.join(DATA_PATH, temp)
+        temp_list = os.listdir(inside)
+        for file in temp_list:
+            if file.endswith(".py"):
+                FINAL_PATH = os.path.join(inside, file)
+                FILE_LIST.append(FINAL_PATH)
+
+else:
+    FILE_LIST_COPY = FILE_LIST[:]
+    FILE_LIST = []
+    for temp in FILE_LIST_COPY:
+        inside = os.path.join(DATA_PATH, temp)
+        FILE_LIST.append(inside)
+    
+
+for PATH in FILE_LIST:
+    print("##########",PATH,"############")
+    os.system(f"python3 {PATH}")
     # os.system("30") #명령어를 입력해야 할 경우
 
-    R = open(file_path, mode = "rt", encoding= "utf-8")
+    R = open(PATH, mode = "rt", encoding= "utf-8")
     print("\n------------CODE--------------")
     for line in R.readlines():
         print(line, end ="")
---
+++ TEST_CASE2/student1/student_one.py
@@ -0,0 +1,4 @@
+#2020310000 정상작동 케이스
+hello = "hello"
+print(hello)
+print(1+2)
